8b7714e75f92ee881055196451ec364e55c48637
RizkyMaulita <rizky.maulita@gmail.com>
2024-01-31 10:15:51 +0700

    add lecture

--- client-mobile/App.js
+++ client-mobile/App.js
@@ -1,14 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
 import {
+  Button,
+  FlatList,
+  Image,
+  ScrollView,
   StyleSheet,
   Text,
   View,
-  Button,
-  Platform,
-  Image,
-  ScrollView,
-  FlatList,
 } from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
@@ -17,70 +16,83 @@ export default function App() {
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((data) => {
-        // console.log(data, "<<< data");
-        setPosts(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
   }, []);
 
+  console.log(posts, "<<< posts");
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
         <View style={styles.container}>
-          {/* <Text style={styles.heading}>Hacktiv8 Phase 3</Text>
+          {/* <Text
+            style={[
+              styles.headerText,
+              styles.styleText,
+              {
+                color: "blue",
+              },
+            ]}
+          >
+            Hacktiv8 Phase 3!
+          </Text>
           <Text>Introduction React Native</Text>
           <Button
-            title={`Button Nih di ${Platform.OS}`}
-            onPress={(event) => {
-              // console.log(event, "<<< event");
-              console.log("button nih ke press");
+            title="Press Me"
+            onPress={() => {
+              console.log("button di click");
             }}
           />
           <StatusBar style="auto" /> */}
-
           <View
             style={{ flex: 1, backgroundColor: "orange", flexDirection: "row" }}
           >
-            <View style={{ flex: 1, backgroundColor: "white" }}>
+            <View style={{ flex: 1, backgroundColor: "#fff" }}>
               <Image
                 style={{
-                  width: "100%",
-                  height: "100%",
+                  width: 150,
+                  height: 120,
+                  resizeMode: "center",
                 }}
                 source={{
                   uri: "https://east.vc/wp-content/uploads/2020/01/hacktiv8-1.png",
                 }}
               />
             </View>
-            <View style={{ flex: 2, backgroundColor: "yellow" }}>
-              <View style={{ padding: 20 }}>
-                <Text style={[styles.heading, { fontStyle: "italic" }]}>
-                  Hacktiv8
+            <View style={{ flex: 2, backgroundColor: "orange" }}>
+              <View style={{ padding: 10 }}>
+                <Text
+                  style={[
+                    styles.headerText,
+                    styles.styleText,
+                    {
+                      color: "blue",
+                    },
+                  ]}
+                >
+                  Hacktiv8 Phase 3!
                 </Text>
-                <Text>Lorem ipsum bla bla bla</Text>
               </View>
             </View>
           </View>
           <View style={{ flex: 3, backgroundColor: "pink" }}>
             <ScrollView>
               {posts.map((post) => (
-                <Text key={post.id} style={{ fontSize: 15, margin: 5 }}>
+                <Text key={post.id} style={{ marginBottom: 10 }}>
                   {" "}
                   - {post.title}
                 </Text>
               ))}
             </ScrollView>
           </View>
-          <View style={{ flex: 3, backgroundColor: "teal" }}>
+          <View style={{ flex: 3, backgroundColor: "yellow" }}>
             <FlatList
               data={posts}
-              renderItem={({ item }) => (
-                <Text style={{ fontSize: 15, margin: 5 }}> - {item.title}</Text>
-              )}
+              renderItem={({ item }) => {
+                return (
+                  <Text style={{ marginBottom: 10 }}> - {item.title}</Text>
+                );
+              }}
               keyExtractor={(item) => item.id}
             />
           </View>
@@ -93,12 +105,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "tomato",
+    backgroundColor: "#fff",
     // alignItems: "center",
     // justifyContent: "center",
   },
-  heading: {
-    fontSize: 20, // dpi dot per inch
+  headerText: {
     fontWeight: "bold",
+    fontSize: 30, // 30 dpi dot per inch
+  },
+  styleText: {
+    color: "red",
+    textDecorationLine: "underline",
   },
 });
